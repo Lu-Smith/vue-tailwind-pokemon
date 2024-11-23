df9b977b34d98130779a44a274e3878f147698a6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-23 21:51:41 +0000

    Created Graphs.vue

---
+++ src/components/Graphs.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  import { Chart, registerables } from 'chart.js';
+  import { ref } from 'vue';
+
+  Chart.register(...registerables);
+
+  const chartRef = ref<Chart | null>(null); 
+  const chartCanvas = ref<HTMLCanvasElement | null>(null);
+</script>
+
+<template>
+  <div>
+    <h1>I am graph</h1>
+    <canvas ref="chartCanvas" class="w-full"></canvas>
+  </div>
+</template>
