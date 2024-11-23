4ccbd251616d9c797c943cf41aa437bea3f025e0
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-23 22:09:58 +0000

    Updated Graphs.vue

--- src/components/Graphs.vue
+++ src/components/Graphs.vue
@@ -1,16 +1,91 @@
 <script setup lang="ts">
   import { Chart, registerables } from 'chart.js';
-  import { ref } from 'vue';
+  import { ref, onMounted } from 'vue';
+
+  defineProps<{ pokemonsExperience: []} >();
 
   Chart.register(...registerables);
 
   const chartRef = ref<Chart | null>(null); 
   const chartCanvas = ref<HTMLCanvasElement | null>(null);
+
+  function renderChart() {
+  // Destroy the previous chart 
+  if (chartRef.value) {
+    chartRef.value.destroy();
+  }
+
+  const ctx = chartCanvas.value?.getContext('2d');
+  if (ctx) {
+    chartRef.value = new Chart(ctx, {
+        type: 'bar', 
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [
+                {
+                    label: 'Pokemon xperience',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: 'rgba(68, 179, 92, 0.6)', 
+                    borderColor: 'rgba(68, 179, 92, 1)',
+                    borderWidth: 1,
+                    type: 'bar',
+                },
+            ],
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    display: true,
+                    position: 'top',
+                    labels: {
+                      font: {
+                        size: window.innerWidth < 640 ? 10 : 12, 
+                      },
+                    },
+                },
+            },
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Name',
+                        font: {
+                          size: window.innerWidth < 640 ? 10 : 14, 
+                        },
+                    },
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Experience',
+                        font: {
+                          size: window.innerWidth < 640 ? 10 : 14, 
+                        },
+                    },
+                    beginAtZero: true,
+                },
+            },
+        },
+    });
+  }
+}
+
+onMounted(() => {
+  renderChart();
+})
 </script>
 
 <template>
   <div>
     <h1>I am graph</h1>
+    <ul>
+      <li v-for="(pokemonData, index) in pokemonsExperience" :key="index">
+        {{ pokemonData.name }}
+        {{ pokemonData.experience }}
+      </li>
+    </ul>
+    
     <canvas ref="chartCanvas" class="w-full"></canvas>
   </div>
 </template>
