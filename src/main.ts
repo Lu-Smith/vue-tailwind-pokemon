62d5bfc5f4c2c4dfd4cf2cb3592784c1c140e38c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-23 21:51:28 +0000

    Swapped to chart.js

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
-import CanvasJSChart from '@canvasjs/vue-charts';
-
 
 const app = createApp(App);
-app.use(CanvasJSChart);
 app.mount('#app');
