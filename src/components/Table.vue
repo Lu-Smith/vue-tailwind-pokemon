8a5abbdf994d235dabb2c22784f5c1041db8701b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-23 22:19:07 +0000

    Created Table.vue

---
+++ src/components/Table.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  defineProps<{ pokemonsExperience: []} >();
+</script>
+
+<template>
+    <h2>I am table</h2>
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Experience</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(pokemonData, index) in pokemonsExperience" :key="index">
+        <td >
+          {{ pokemonData.name }}
+        </td>
+        <td >
+          {{ pokemonData.experience }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
