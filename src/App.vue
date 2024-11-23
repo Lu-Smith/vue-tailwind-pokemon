cb037375f5d00fa7c55287d90e878325d2ba8e19
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-23 17:21:33 +0000

    Installed axios and vue charts

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
-<script setup lang="ts">
+<script lang="ts">
+
+export default {
+data() {
+	return {
+		chart: null,
+		options: {
+		animationEnabled: true,
+		title:{
+			text: "Pokemons"
+		},
+		data: [{
+			type: "column",
+			dataPoints: [
+			{ label: "apple",  y: 10 },
+			{ label: "orange", y: 15 },
+			{ label: "banana", y: 25 },
+			{ label: "mango",  y: 30 },
+			{ label: "grape",  y: 28 }
+			]
+		}]
+		}
+	}
+}
+}
 
 </script>
 
 <template>
   <h1>Hello World</h1>
+  <CanvasJSChart :options="options"/>
 </template>
 
-<style scoped>
-
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import CanvasJSChart from '@canvasjs/vue-charts';
 
-createApp(App).mount('#app')
+
+const app = createApp(App);
+app.use(CanvasJSChart);
+app.mount('#app');
