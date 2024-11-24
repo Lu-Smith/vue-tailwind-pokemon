341da6437fecc18b29950c8240d82c819c31c7e8
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-24 18:11:36 +0000

    Updated index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,8 @@ import { createWebHistory, createRouter } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import PokemonGraphsView from '../views/PokemonGraphsView.vue';
 import PokemonTablesView from '../views/PokemonTablesView.vue';
+import BenchmarksGraphsView from '../views/BenchamarksGraphs.vue';
+import BenchmarksTablesView from '../views/BenchmarksTablesView.vue';
 import NotFoundView from '../views/NotFoundView.vue';
 
 const router = createRouter({
@@ -22,6 +24,16 @@ const router = createRouter({
       path: '/pokemon-tables',
       name: 'pokemon-tables',
       component: PokemonTablesView
+    },
+    {
+      path: '/benchmarks-graphs',
+      name: 'benchmarks-graphs',
+      component: BenchmarksGraphsView
+    },
+    {
+      path: '/benchmarks-tables',
+      name: 'benchmarks-tables',
+      component: BenchmarksTablesView
     },
     {
       path: '/:catchAll(.*)',
