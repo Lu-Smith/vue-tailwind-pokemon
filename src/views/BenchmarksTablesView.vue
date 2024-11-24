8781e0b50e25c1be4d096217492ee60c358c0005
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-24 19:06:06 +0000

    Updated BenchmarkTablesView.vue

--- src/views/BenchmarksTablesView.vue
+++ src/views/BenchmarksTablesView.vue
@@ -3,11 +3,36 @@
   import { getProductBenchmarks } from '../data/benchmarksApi';
   import ProductBenchmarks from '../assets/interfaces';
 
+  const headTitles = ref(['ID', 'Provider Name', 'Provider Name', 
+  'Benchmark', 'Payment', 'Date', 'Period Length']);
+ 
   const benchmarksData = ref<ProductBenchmarks[]>([]);
 
   const processBenchmarksData = (benchmarks: ProductBenchmarks[]) => {
     console.log(benchmarks);
-    return benchmarks;
+    const newBenchmarks: ProductBenchmarks[] = [];
+
+    for (const benchmark of benchmarks) {
+      const {id, provider_name, product_name, 
+        currency, benchmark: benchmarkValue, payment, 
+        start_date, end_date} = benchmark;
+
+      newBenchmarks.push({
+      id,
+      provider_name,
+      product_name,
+      payment,
+      benchmark: benchmarkValue,
+      start_date,
+      end_date,
+      currency,
+    });
+    
+    };
+
+
+
+    return newBenchmarks;
   }
 
   onMounted(async () =>{
@@ -23,6 +48,21 @@
 
 
 <template>
-  I am Benchmarks Tables
-
+  <table>
+    <thead>
+      <tr>
+        <th v-for="(title, index) in headTitles" :key="index">{{ title }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="benchmark in benchmarksData" :key="benchmark.id">
+        <td>{{ benchmark.id }}</td>
+        <td>{{ benchmark.provider_name }}</td>
+        <td>{{ benchmark.product_name }}</td>
+        <td>{{ benchmark.benchmark }}</td>
+        <td>{{ benchmark.payment }}</td>
+        <td>{{ benchmark.start_date }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
