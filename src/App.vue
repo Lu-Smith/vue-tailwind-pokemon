125d73bcdf6262aa81b2912274c3176df53d65a6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-24 15:26:32 +0000

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <script  setup lang="ts">
+  import { onMounted } from 'vue';
   import NavBar from './components/NavBar.vue';
   import { RouterView } from 'vue-router';
+  import { fetchPokemonData } from './data/pokemonApi';
+
+  onMounted(() =>{
+    fetchPokemonData();
+  });
 </script>
 
 <template>
