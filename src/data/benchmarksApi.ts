f54d5cd7c78bbd0379105e9a4b0f5b7291b49a36
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-24 18:11:18 +0000

    Updated benchmarksApi.ts

--- src/data/benchmarksApi.ts
+++ src/data/benchmarksApi.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import ProductBenchmarks from '../assets/interfaces';
 
 const BASE_URL_BENCHMARKS = 'https://substantive.pythonanywhere.com';
 const API_KEY_BENCHMARKS = '590e3e17b6a26a8fcda726e2a91520e476e2c894';
@@ -11,15 +12,13 @@ const apiBenchmarks = axios.create({
   },
 });
 
-
-
-const productBenchmarks = async () => {
-  const response = await apiBenchmarks.get('product_benchmarks');
+const getProductBenchmarks = async () => {
+  const response = await apiBenchmarks.get<{product_benchmarks: ProductBenchmarks[]}>('product_benchmarks');
   console.log(response.data);
   return response.data;
 }
 
-export {productBenchmarks};
+export {getProductBenchmarks};
 
 
 
