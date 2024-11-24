50dbc4f50004078600cb68b219d098ff97eaacab
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-24 15:15:49 +0000

    Moved api data to pokemonApi.ts

---
+++ src/data/pokemonApi.ts
@@ -0,0 +1,30 @@
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const pokemonNames = ref<string[]>(['ditto', 'abra', 'absol', 'aggron', 'altaria', 'bagon', 'breloom' ])
+
+const pokemonsData = ref<any[]>([]);
+const pokemonExperiencesData = ref<number[]>([]);
+const pokemonNamesData = ref<string[]>([]);
+
+onMounted(async () => {
+  try {
+    const responses = await Promise.all(
+      pokemonNames.value.map((name) =>
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      )
+    );
+
+    pokemonsData.value = responses.map((response) => response.data);
+
+    pokemonExperiencesData.value = pokemonsData.value.map(pokemon => pokemon.base_experience);
+    pokemonNamesData.value = pokemonsData.value.map(pokemon => pokemon.name);
+
+  } catch (error) {
+    console.error('Error fetching Pokémon data:', error);
+  }
+});
+
+export { pokemonsData, pokemonExperiencesData, pokemonNamesData };
+
+
