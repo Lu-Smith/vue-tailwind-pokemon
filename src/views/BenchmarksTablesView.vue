60136ce1789f2b9a860509a44c5b7f5d524ad6d6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-24 18:12:10 +0000

    Created BenchmarksTablesView.vue

---
+++ src/views/BenchmarksTablesView.vue
@@ -0,0 +1,28 @@
+<script  setup lang="ts">
+  import { onMounted, ref } from 'vue';
+  import { getProductBenchmarks } from '../data/benchmarksApi';
+  import ProductBenchmarks from '../assets/interfaces';
+
+  const benchmarksData = ref<ProductBenchmarks[]>([]);
+
+  const processBenchmarksData = (benchmarks: ProductBenchmarks[]) => {
+    console.log(benchmarks);
+    return benchmarks;
+  }
+
+  onMounted(async () =>{
+    try{
+      const produckBenchmarks = await getProductBenchmarks();
+
+      benchmarksData.value = processBenchmarksData(produckBenchmarks.product_benchmarks);
+    } catch(error) {
+      console.error('Failed to fetch data:', error);
+    }
+  });
+</script>
+
+
+<template>
+  I am Benchmarks Tables
+
+</template>
