38a8bbd4a7e5dd2f55a3101ca959ad0deb493c4c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-24 15:18:06 +0000

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,12 @@
 <script  setup lang="ts">
- import { ref, onMounted } from 'vue';
- import axios from 'axios';
- import Graphs from './components/Graphs.vue';
- import Table from './components/Table.vue';
-
- const pokemonNames = ref<string[]>(['ditto', 'abra', 'absol', 'aggron', 'altaria', 'bagon', 'breloom' ])
-
- const pokemonsData = ref<any[]>([]);
- const pokemonExperiencesData = ref<number[]>([]);
- const pokemonNamesData = ref<string[]>([]);
-
- onMounted(async () => {
-  try {
-    const responses = await Promise.all(
-      pokemonNames.value.map((name) =>
-        axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
-      )
-    );
-
-    pokemonsData.value = responses.map((response) => response.data);
-
-    pokemonExperiencesData.value = pokemonsData.value.map(pokemon => pokemon.base_experience);
-    pokemonNamesData.value = pokemonsData.value.map(pokemon => pokemon.name);
-
-  } catch (error) {
-    console.error('Error fetching Pokémon data:', error);
-  }
-});
-
-
+  import NavBar from './components/NavBar.vue';
+  import { RouterView } from 'vue-router';
 </script>
 
 <template>
   <div class="p-4 text-center flex justify-center flex-col items-center">
-    <h1 class="font-bold mb-5">Hello World</h1>
-    <Graphs :pokemonExperiencesData="pokemonExperiencesData" :pokemonNamesData="pokemonNamesData" />
-    <Table :pokemonsData="pokemonsData" />
+    <NavBar />
+    <RouterView />
   </div>
 </template>
 
